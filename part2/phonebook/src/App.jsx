014873e9375a6d7df701643670b16dce1f06fb1f
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-03-21 16:42:03 -0300

    added json server and axios fetch

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Filter = ({ filter, handleFilter }) => {
   <div>
@@ -50,15 +51,17 @@ const Persons = ({ persons, filter }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState();
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((res) => {
+      const data = res.data;
+      setPersons(data);
+    });
+  }, []);
 
   const handleFilter = (event) => {
     setFilter(event.target.value);
@@ -94,7 +97,7 @@ const App = () => {
         handleSubmit={handleSubmit}
       />
       <h2>Numbers</h2>
-      <Persons persons={persons} filter={filter} />
+      {persons ? <Persons persons={persons} filter={filter} /> : null}
     </div>
   );
 };
