e0af33db00509a82e04fa624793b2834e330ed31
Manuel Miglioranza <mmiglioranza22@gmail.com>
2024-04-11 16:21:32 -0300

    4.14-4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -20,4 +20,24 @@ blogRouter.post("/", async (request, response, next) => {
   }
 });
 
+blogRouter.delete("/:id", async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id);
+    response.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogRouter.patch("/:id", async (request, response, next) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(request.params.id, request.body, {
+      new: true,
+    });
+    response.status(200).json(blog);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = blogRouter;
--- part4/tests/note_api.test.js
+++ part4/tests/note_api.test.js
@@ -28,96 +28,124 @@ const initialBlogs = [
   },
 ];
 
-describe("api", () => {
+describe("API tests suite:", () => {
   beforeEach(async () => {
     await Blog.deleteMany({});
     await Promise.all(initialBlogs.map((blog) => new Blog(blog).save()));
   });
-  it("fetches blogs correctly", async () => {
-    await api
-      .get("/api/blogs")
-      .expect(200)
-      .expect((res) => {
+  describe("1- fetching posts", () => {
+    it("fetches blogs correctly", async () => {
+      await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect((res) => {
+          assert.strictEqual(res.body.length, initialBlogs.length);
+        })
+        .expect("Content-Type", /application\/json/);
+    });
+    it("_id prop exists in each blog from DB", async () => {
+      await api.get("/api/blogs").expect((res) => {
+        res.body.forEach((doc) => {
+          assert.ok(doc._id);
+        });
+      });
+    });
+  });
+  describe("2- creating posts", () => {
+    it("creates a new post correctly in DB", async () => {
+      const newBlog = {
+        title: "new",
+        author: "blog",
+        url: "123",
+        likes: 1,
+      };
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect((res) => {
+          assert.ok(res.body._id);
+          assert.strictEqual(res.body.title, newBlog.title);
+          assert.strictEqual(res.body.author, newBlog.author);
+        });
+      await api.get("/api/blogs").expect((res) => {
+        assert.strictEqual(res.body.length, 4);
+      });
+    });
+    it("likes default set to 0 when not passed in body payload", async () => {
+      const newBlog = {
+        title: "no",
+        author: "likes",
+        url: "no likes",
+      };
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect((res) => {
+          assert.ok(res.body._id);
+          assert.strictEqual(res.body.title, newBlog.title);
+          assert.strictEqual(res.body.author, newBlog.author);
+          assert.strictEqual(res.body.likes, 0);
+        });
+    });
+    it("return error if title or author are not sent in payload", async () => {
+      const newBlog = {
+        url: "no author nor title",
+      };
+      await api.post("/api/blogs").send(newBlog).expect(400);
+      const noTitle = {
+        author: "no title",
+      };
+      await api
+        .post("/api/blogs")
+        .send(noTitle)
+        .expect(400)
+        .expect((res) => {
+          assert(res.body.message.includes("Path `title` is required"));
+        });
+
+      const noAuthor = {
+        title: "no author",
+      };
+      await api
+        .post("/api/blogs")
+        .send(noAuthor)
+        .expect(400)
+        .expect((res) => {
+          assert(res.body.message.includes("Path `author` is required"));
+        });
+    });
+  });
+  describe("3- deleting posts", () => {
+    it("deletes a post correctly by its _id", async () => {
+      let id;
+      await api.get("/api/blogs").expect((res) => {
+        id = res.body[0]._id;
+      });
+      await api.delete(`/api/blogs/${id}`).expect(204);
+      await api.get("/api/blogs").expect((res) => {
+        assert.strictEqual(res.body.length, initialBlogs.length - 1);
+      });
+    });
+  });
+  describe("4- updating posts", () => {
+    it("updates a post correctly by its _id", async () => {
+      let id;
+      let title = "updated title";
+
+      await api.get("/api/blogs").expect((res) => {
+        id = res.body[0]._id;
+      });
+      await api
+        .patch(`/api/blogs/${id}`)
+        .send({ title: title })
+        .expect(200)
+        .expect((res) => {
+          assert.strictEqual(res.body.title, title);
+        });
+      await api.get("/api/blogs").expect((res) => {
         assert.strictEqual(res.body.length, initialBlogs.length);
-      })
-      .expect("Content-Type", /application\/json/);
-  });
-  it("_id prop exists in each blog from DB", async () => {
-    await api.get("/api/blogs").expect((res) => {
-      res.body.forEach((doc) => {
-        assert.ok(doc._id);
       });
     });
-  });
-
-  it("creates a new post correctly in DB", async () => {
-    const newBlog = {
-      title: "new",
-      author: "blog",
-      url: "123",
-      likes: 1,
-    };
-    await api
-      .post("/api/blogs")
-      .send(newBlog)
-      .expect((res) => {
-        assert.ok(res.body._id);
-        assert.strictEqual(res.body.title, newBlog.title);
-        assert.strictEqual(res.body.author, newBlog.author);
-      });
-    await api.get("/api/blogs").expect((res) => {
-      assert.strictEqual(res.body.length, 4);
-    });
-  });
-
-  it("likes default set to 0 when not passed in body payload", async () => {
-    const newBlog = {
-      title: "no",
-      author: "likes",
-      url: "no likes",
-    };
-    await api
-      .post("/api/blogs")
-      .send(newBlog)
-      .expect((res) => {
-        assert.ok(res.body._id);
-        assert.strictEqual(res.body.title, newBlog.title);
-        assert.strictEqual(res.body.author, newBlog.author);
-        assert.strictEqual(res.body.likes, 0);
-      });
-  });
-  it("return error if title or author are not sent in payload", async () => {
-    const newBlog = {
-      url: "no author nor title",
-    };
-    await api.post("/api/blogs").send(newBlog).expect(400);
-    const noTitle = {
-      author: "no title",
-    };
-    await api
-      .post("/api/blogs")
-      .send(noTitle)
-      .expect(400)
-      .expect((res) => {
-        assert.strictEqual(
-          true,
-          res.body.message.includes("Path `title` is required")
-        );
-      });
-
-    const noAuthor = {
-      title: "no author",
-    };
-    await api
-      .post("/api/blogs")
-      .send(noAuthor)
-      .expect(400)
-      .expect((res) => {
-        assert.strictEqual(
-          true,
-          res.body.message.includes("Path `author` is required")
-        );
-      });
   });
 
   after(async () => {
